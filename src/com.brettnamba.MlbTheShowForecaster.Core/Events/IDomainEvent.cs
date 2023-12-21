60defd1b5178066c9bbf4398fd88f5b5654e2d55
bretten <brettinbox@gmail.com>
2023-12-21 04:19:59 -0800

    #1 Adding Core with Domain events and SeedWork

---
+++ src/com.brettnamba.MlbTheShowForecaster.Core/Events/IDomainEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Core.Events;
+
+/// <summary>
+/// Represents a business critical event that has occurred in the domain and is relevant to another part of the domain
+/// </summary>
+public interface IDomainEvent
+{
+}
