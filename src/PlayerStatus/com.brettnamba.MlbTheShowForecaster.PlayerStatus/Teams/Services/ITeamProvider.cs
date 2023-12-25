b330c6f0ebd4013ab43e1ce8ba0500d9d7ef295a
bretten <brettinbox@gmail.com>
2023-12-24 17:19:59 -0800

    #3 Adding Player repository and Team provider service contracts

---
+++ src/PlayerStatus/com.brettnamba.MlbTheShowForecaster.PlayerStatus/Teams/Services/ITeamProvider.cs
@@ -0,0 +1,24 @@
+﻿using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Common.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Teams.ValueObjects;
+
+namespace com.brettnamba.MlbTheShowForecaster.PlayerStatus.Teams.Services;
+
+/// <summary>
+/// Service that provides Teams based on different parameters
+/// </summary>
+public interface ITeamProvider
+{
+    /// <summary>
+    /// Gets a <see cref="Team"/> by <see cref="MlbId"/>
+    /// </summary>
+    /// <param name="mlbId">The <see cref="MlbId"/></param>
+    /// <returns>The <see cref="Team"/> with the <see cref="MlbId"/>, otherwise null</returns>
+    Team? GetBy(MlbId mlbId);
+
+    /// <summary>
+    /// Gets a <see cref="Team"/> by <see cref="TeamAbbreviation"/>
+    /// </summary>
+    /// <param name="abbreviation">The <see cref="TeamAbbreviation"/></param>
+    /// <returns>The <see cref="Team"/> with the <see cref="TeamAbbreviation"/>, otherwise null</returns>
+    Team? GetBy(TeamAbbreviation abbreviation);
+}
