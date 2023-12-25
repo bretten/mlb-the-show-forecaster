ac800110e7d87237cd3ad6f1b85330197b23b0f9
Brett <bretten@users.noreply.github.com>
2023-12-25 04:24:28 -0800

    #4 Service to detect if a Player's status changes (#15)

---
+++ src/PlayerStatus/com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain/Players/Services/PlayerStatusChangeDetector.cs
@@ -0,0 +1,76 @@
+﻿using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Entities;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Enums;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Teams.ValueObjects;
+
+namespace com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Services;
+
+/// <summary>
+/// Detects <see cref="Player"/> status changes by comparing the <see cref="Player"/> to the current MLB reported states
+/// </summary>
+public sealed class PlayerStatusChangeDetector : IPlayerStatusChangeDetector
+{
+    /// <summary>
+    /// Detects status changes for the <see cref="Player"/> based on the specified parameters
+    /// </summary>
+    /// <param name="player">The <see cref="Player"/></param>
+    /// <param name="mlbReportedActiveStatus">True if MLB reports the <see cref="Player"/> as active</param>
+    /// <param name="mlbReportedTeam">The <see cref="Team"/> that the MLB reports the <see cref="Player"/> is currently on</param>
+    /// <returns>Any status changes</returns>
+    public PlayerStatusChanges DetectChanges(Player player, bool mlbReportedActiveStatus,
+        Team? mlbReportedTeam)
+    {
+        var changes = new List<PlayerStatusChangeType>();
+
+        CheckForActiveStatusChanges(player, mlbReportedActiveStatus, ref changes);
+
+        CheckForTeamChanges(player, mlbReportedTeam, ref changes);
+
+        return new PlayerStatusChanges(changes, mlbReportedTeam);
+    }
+
+    /// <summary>
+    /// Checks if the <see cref="Player"/>'s active status has changed and if so, adds any status changes
+    /// to the collection of changes
+    /// </summary>
+    /// <param name="player">The <see cref="Player"/></param>
+    /// <param name="mlbReportedActiveStatus">True if MLB reports the <see cref="Player"/> as active</param>
+    /// <param name="changes">A collection of changes to add to</param>
+    private static void CheckForActiveStatusChanges(Player player, bool mlbReportedActiveStatus,
+        ref List<PlayerStatusChangeType> changes)
+    {
+        switch (player.Active)
+        {
+            case false when mlbReportedActiveStatus:
+                changes.Add(PlayerStatusChangeType.Activated);
+                break;
+            case true when !mlbReportedActiveStatus:
+                changes.Add(PlayerStatusChangeType.Inactivated);
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Checks if the <see cref="Player"/>'s <see cref="Team"/> has changed. If so, any status changes are recorded
+    /// in the collection of changes
+    /// </summary>
+    /// <param name="player">The <see cref="Player"/></param>
+    /// <param name="mlbReportedTeam">The <see cref="Team"/> that the MLB reports the <see cref="Player"/> is currently on</param>
+    /// <param name="changes">A collection of changes to add to</param>
+    private static void CheckForTeamChanges(Player player, Team? mlbReportedTeam,
+        ref List<PlayerStatusChangeType> changes)
+    {
+        if (!player.IsFreeAgent() && mlbReportedTeam == null)
+        {
+            changes.Add(PlayerStatusChangeType.EnteredFreeAgency);
+        }
+        else if (player.IsFreeAgent() && mlbReportedTeam != null)
+        {
+            changes.Add(PlayerStatusChangeType.SignedWithNewTeam);
+        }
+        else if (player.Team != mlbReportedTeam)
+        {
+            changes.Add(PlayerStatusChangeType.Traded);
+        }
+    }
+}
---
+++ test/PlayerStatus/com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Tests/Players/Services/PlayerStatusChangeDetectorTests.cs
@@ -0,0 +1,90 @@
+﻿using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Enums;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Services;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Tests.Players.TestClasses;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Tests.Teams.TestClasses;
+
+namespace com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Tests.Players.Services;
+
+public class PlayerStatusChangeDetectorTests
+{
+    [Fact]
+    public void DetectChanges_InactivePlayerReportedAsActive_DetectsActive()
+    {
+        // Arrange
+        var detector = new PlayerStatusChangeDetector();
+        var player = PlayerMocker.Mock(active: false);
+        var mlbReportedActiveStatus = true;
+
+        // Act
+        var actual = detector.DetectChanges(player, mlbReportedActiveStatus, null);
+
+        // Assert
+        Assert.Contains(PlayerStatusChangeType.Activated, actual.Changes);
+    }
+
+    [Fact]
+    public void DetectChanges_ActivePlayerReportedAsInactive_DetectsInactive()
+    {
+        // Arrange
+        var detector = new PlayerStatusChangeDetector();
+        var player = PlayerMocker.Mock(active: true);
+        var mlbReportedActiveStatus = false;
+
+        // Act
+        var actual = detector.DetectChanges(player, mlbReportedActiveStatus, null);
+
+        // Assert
+        Assert.Contains(PlayerStatusChangeType.Inactivated, actual.Changes);
+    }
+
+    [Fact]
+    public void DetectChanges_PlayerOnTeamReportedAsFreeAgent_DetectsFreeAgency()
+    {
+        // Arrange
+        var detector = new PlayerStatusChangeDetector();
+        var currentTeam = TeamMocker.Mock();
+        var player = PlayerMocker.Mock(team: currentTeam);
+        var mlbReportedActiveStatus = true;
+        var mlbReportedTeam = TeamMocker.NoTeam;
+
+        // Act
+        var actual = detector.DetectChanges(player, mlbReportedActiveStatus, mlbReportedTeam);
+
+        // Assert
+        Assert.Contains(PlayerStatusChangeType.EnteredFreeAgency, actual.Changes);
+    }
+
+    [Fact]
+    public void DetectChanges_FreeAgentPlayerReportedOnANewTeam_DetectsNewTeamSigning()
+    {
+        // Arrange
+        var detector = new PlayerStatusChangeDetector();
+        var currentTeam = TeamMocker.NoTeam;
+        var player = PlayerMocker.Mock(team: currentTeam);
+        var mlbReportedActiveStatus = true;
+        var mlbReportedTeam = TeamMocker.Mock();
+
+        // Act
+        var actual = detector.DetectChanges(player, mlbReportedActiveStatus, mlbReportedTeam);
+
+        // Assert
+        Assert.Contains(PlayerStatusChangeType.SignedWithNewTeam, actual.Changes);
+    }
+
+    [Fact]
+    public void DetectChanges_PlayerOnTeamReportedOnDifferentTeam_DetectsTrade()
+    {
+        // Arrange
+        var detector = new PlayerStatusChangeDetector();
+        var currentTeam = TeamMocker.Mock();
+        var player = PlayerMocker.Mock(team: currentTeam);
+        var mlbReportedActiveStatus = true;
+        var mlbReportedTeam = TeamMocker.Mock(1000, "New Team", "NEW");
+
+        // Act
+        var actual = detector.DetectChanges(player, mlbReportedActiveStatus, mlbReportedTeam);
+
+        // Assert
+        Assert.Contains(PlayerStatusChangeType.Traded, actual.Changes);
+    }
+}
