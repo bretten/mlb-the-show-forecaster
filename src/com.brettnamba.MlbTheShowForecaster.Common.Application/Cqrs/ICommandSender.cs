9b4e668e4e76370b77ef651d9952b4fdb68c3962
Brett <bretten@users.noreply.github.com>
2023-12-25 16:31:53 -0800

    #5 Defining contracts for CQRS and object mapping (#16)
    * #5 Defining contracts for CQRS
    
    * #6 Adding object mapping contract

---
+++ src/com.brettnamba.MlbTheShowForecaster.Common.Application/Cqrs/ICommandSender.cs
@@ -0,0 +1,16 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Application.Cqrs;
+
+/// <summary>
+/// Defines a sender for <see cref="ICommand"/>
+/// </summary>
+public interface ICommandSender
+{
+    /// <summary>
+    /// Sends the specified command to the corresponding handler
+    /// </summary>
+    /// <param name="command">The command to send</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete</param>
+    /// <typeparam name="TCommand">The command type that implements <see cref="ICommand"/></typeparam>
+    /// <returns>The completed task</returns>
+    Task Send<TCommand>(TCommand command, CancellationToken cancellationToken = default) where TCommand : ICommand;
+}
---
+++ src/com.brettnamba.MlbTheShowForecaster.Common.Application/Cqrs/IQuerySender.cs
@@ -0,0 +1,16 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Application.Cqrs;
+
+/// <summary>
+/// Defines a <see cref="IQuery{TResponse}"/> sender
+/// </summary>
+public interface IQuerySender
+{
+    /// <summary>
+    /// Sends the specified query to the corresponding handler
+    /// </summary>
+    /// <param name="query">The query to send</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete</param>
+    /// <typeparam name="TResponse">The query response type</typeparam>
+    /// <returns>The response of the query</returns>
+    Task<TResponse?> Send<TResponse>(IQuery<TResponse> query, CancellationToken cancellationToken = default);
+}
