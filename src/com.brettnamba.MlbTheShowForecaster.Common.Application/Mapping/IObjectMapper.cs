492bb15ec8685250ca51b71c8741159d751c9f48
bretten <brettinbox@gmail.com>
2023-12-25 16:29:25 -0800

    #6 Adding object mapping contract

---
+++ src/com.brettnamba.MlbTheShowForecaster.Common.Application/Mapping/IObjectMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Application.Mapping;
+
+/// <summary>
+/// Defines a mapper that can map an object to different type of object
+/// </summary>
+public interface IObjectMapper
+{
+    /// <summary>
+    /// Maps an object of type TIn to type TOut
+    /// </summary>
+    /// <param name="source">The object to map</param>
+    /// <typeparam name="TIn">The type of the object to map</typeparam>
+    /// <typeparam name="TOut">The type of the resulting object</typeparam>
+    /// <returns>The resulting object</returns>
+    TOut Map<TIn, TOut>(TIn source);
+}
