eb6a7ea4a62b0388b5adb733f4554744cbe974b9
bretten <brettinbox@gmail.com>
2023-12-30 01:21:22 -0800

    #18 Added a library for common Infrastructure modules, with an EF implementation of UnitOfWork

---
+++ src/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure/EntityFramework/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Core.SeedWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.EntityFramework;
+
+/// <summary>
+/// An EF implementation of unit of work that forces any changes to the DB context (since the last call to commit)
+/// to be encapsulated as a single, logical unit of work by only saving changes when commit is invoked
+/// </summary>
+/// <typeparam name="TDbContext"></typeparam>
+public sealed class UnitOfWork<TDbContext> : IUnitOfWork, IDisposable, IAsyncDisposable where TDbContext : DbContext
+{
+    /// <summary>
+    /// The DB context
+    /// </summary>
+    private readonly TDbContext _dbContext;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="dbContext">The DB context</param>
+    public UnitOfWork(TDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    /// <summary>
+    /// Saves changes to the database
+    ///
+    /// <para>Commit is meant to be invoked after all changes that represent a unit of work are made on the
+    /// <see cref="DbContext"/> so that they are encapsulated in a logical transaction</para>
+    /// </summary>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete</param>
+    public async Task CommitAsync(CancellationToken cancellationToken = default)
+    {
+        await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// Disposes of the DB context
+    /// </summary>
+    public void Dispose()
+    {
+        _dbContext.Dispose();
+    }
+
+    /// <summary>
+    /// Disposes of the DB context
+    /// </summary>
+    public async ValueTask DisposeAsync()
+    {
+        await _dbContext.DisposeAsync();
+    }
+}
---
+++ test/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests/EntityFramework/UnitOfWorkTests.cs
@@ -0,0 +1,51 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.EntityFramework;
+using com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.EntityFramework.TestClasses;
+using Moq;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.EntityFramework;
+
+public class UnitOfWorkTests
+{
+    [Fact]
+    public async Task CommitAsync_NoParams_SaveChangesAsyncInvoked()
+    {
+        // Arrange
+        var cToken = CancellationToken.None;
+        var mockDbContext = Mock.Of<TestDbContext>(x => x.SaveChangesAsync(cToken) == Task.FromResult(1));
+        var uow = new UnitOfWork<TestDbContext>(mockDbContext);
+
+        // Act
+        await uow.CommitAsync(cToken);
+
+        // Assert
+        Mock.Get(mockDbContext).Verify(x => x.SaveChangesAsync(cToken), Times.Once);
+    }
+
+    [Fact]
+    public void Dispose_NoParams_DbContextDisposed()
+    {
+        // Arrange
+        var mockDbContext = Mock.Of<TestDbContext>();
+        var uow = new UnitOfWork<TestDbContext>(mockDbContext);
+
+        // Act
+        uow.Dispose();
+
+        // Assert
+        Mock.Get(mockDbContext).Verify(x => x.Dispose(), Times.Once);
+    }
+
+    [Fact]
+    public async Task DisposeAsync_NoParams_DbContextDisposed()
+    {
+        // Arrange
+        var mockDbContext = Mock.Of<TestDbContext>();
+        var uow = new UnitOfWork<TestDbContext>(mockDbContext);
+
+        // Act
+        await uow.DisposeAsync();
+
+        // Assert
+        Mock.Get(mockDbContext).Verify(x => x.DisposeAsync(), Times.Once);
+    }
+}
