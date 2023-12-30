a8d232596d6494058a542b7fa2a720eba215fb68
Brett <bretten@users.noreply.github.com>
2023-12-30 01:54:55 -0800

    #20 AutoMapper implementation of IObjectMapper (#28)

---
+++ test/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests/Mapping/AutoMapper/TestClasses/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.Mapping.AutoMapper.TestClasses;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<ObjectTypeA, ObjectTypeB>()
+            .ForMember(dest => dest.IntegerValue, opt => opt.MapFrom(src => src.AnInt))
+            .ForMember(dest => dest.StringValue, opt => opt.MapFrom(src => src.AString))
+            .ConstructUsing(x => new ObjectTypeB(x.AnInt, x.AString));
+    }
+}
---
+++ test/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests/Mapping/AutoMapper/TestClasses/ObjectTypeB.cs
@@ -0,0 +1,14 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.Mapping.AutoMapper.TestClasses;
+
+public sealed class ObjectTypeB
+{
+    public ObjectTypeB(int integerValue, string stringValue)
+    {
+        IntegerValue = integerValue;
+        StringValue = stringValue;
+    }
+
+    public int IntegerValue { get; private set; }
+
+    public string StringValue { get; private set; }
+}
