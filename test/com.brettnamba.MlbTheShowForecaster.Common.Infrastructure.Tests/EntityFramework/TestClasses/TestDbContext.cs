c92962c7622f8462c460e0e55c0d938dd815c4c4
Brett <bretten@users.noreply.github.com>
2023-12-30 01:23:33 -0800

    #18 Added a library for common Infrastructure modules, with an EF implementation of UnitOfWork (#27)

---
+++ test/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests/EntityFramework/TestClasses/TestDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.EntityFramework.TestClasses;
+
+public class TestDbContext : DbContext
+{
+    public DbSet<TestEntity>? TestEntities { get; set; }
+}
---
+++ test/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests/EntityFramework/TestClasses/TestEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.EntityFramework.TestClasses;
+
+public class TestEntity
+{
+    private TestEntity(int integerValue, string stringValue)
+    {
+        IntegerValue = integerValue;
+        StringValue = stringValue;
+    }
+
+    public int IntegerValue { get; private set; }
+
+    public string StringValue { get; private set; }
+
+    public static TestEntity Create(int integerValue, string stringValue)
+    {
+        return new TestEntity(integerValue, stringValue);
+    }
+}
