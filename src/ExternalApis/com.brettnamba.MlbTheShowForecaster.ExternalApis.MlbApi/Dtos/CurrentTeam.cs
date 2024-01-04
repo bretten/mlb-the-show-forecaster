2ad09d914e114e17ed3c77a2d4206029c4ba47e0
bretten <brettinbox@gmail.com>
2024-01-03 22:06:21 -0800

    #26 Added MLB API client

---
+++ src/ExternalApis/com.brettnamba.MlbTheShowForecaster.ExternalApis.MlbApi/Dtos/CurrentTeam.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace com.brettnamba.MlbTheShowForecaster.ExternalApis.MlbApi.Dtos;
+
+/// <summary>
+/// A player's current team
+/// </summary>
+/// <param name="Id">The team's MLB ID</param>
+public readonly record struct CurrentTeam([property: JsonPropertyName("id")] int Id);
