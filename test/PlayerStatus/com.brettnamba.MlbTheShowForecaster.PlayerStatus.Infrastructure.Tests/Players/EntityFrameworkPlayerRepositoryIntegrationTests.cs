71df68f5625c3ff03076f1852bb7d807e5459184
Brett <bretten@users.noreply.github.com>
2024-01-06 15:13:02 -0800

    #30 Added PlayerStatus infrastructure layer with EF integration (#33)
    * #30 Added PlayerStatus infrastructure layer with EF integration
    
    * #30 Adding tests for PlayersDbContext and Repo
    
    * #30 Doc for integration tests, removing unneeded save

---
+++ test/PlayerStatus/com.brettnamba.MlbTheShowForecaster.PlayerStatus.Infrastructure.Tests/Players/EntityFrameworkPlayerRepositoryIntegrationTests.cs
@@ -0,0 +1,148 @@
+﻿using System.Data.Common;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Teams.Services;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Infrastructure.Players.EntityFramework;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Infrastructure.Tests.TestClasses;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Npgsql;
+using Testcontainers.PostgreSql;
+
+namespace com.brettnamba.MlbTheShowForecaster.PlayerStatus.Infrastructure.Tests.Players;
+
+/// <summary>
+/// Integration tests that use TestContainers to spin up database containers for the tests. Requires Docker to be
+/// running for these tests
+/// </summary>
+public class EntityFrameworkPlayerRepositoryIntegrationTests : IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _container;
+
+    /// <summary>
+    /// Configures the container options that will be used for each test
+    /// </summary>
+    /// <exception cref="DockerNotRunningException">Thrown if Docker is not running</exception>
+    public EntityFrameworkPlayerRepositoryIntegrationTests()
+    {
+        try
+        {
+            _container = new PostgreSqlBuilder()
+                .WithName(GetType().Name + Guid.NewGuid())
+                .WithUsername("postgres")
+                .WithPassword("password99")
+                .WithPortBinding(54321, 5432)
+                .Build();
+        }
+        catch (ArgumentException e)
+        {
+            if (!e.Message.Contains("Docker is either not running or misconfigured"))
+            {
+                throw;
+            }
+
+            throw new DockerNotRunningException($"Docker is required to run tests for {GetType().Name}");
+        }
+    }
+
+    [Fact]
+    [Trait("Category", "Integration")]
+    public async Task Add_Player_AddsPlayerToDbContextSet()
+    {
+        // Arrange
+        var fakeTeam = Faker.FakeTeam();
+        var fakePlayer = Faker.FakePlayer(team: fakeTeam);
+        var stubTeamProvider = Mock.Of<ITeamProvider>(x => x.GetBy(fakeTeam.Abbreviation) == fakeTeam);
+
+        await using var connection = await GetDbConnection();
+        await using var dbContext = GetDbContext(connection, stubTeamProvider);
+        await dbContext.Database.MigrateAsync();
+        var repo = new EntityFrameworkPlayerRepository(dbContext);
+
+        // Act
+        await repo.Add(fakePlayer);
+        await dbContext.SaveChangesAsync();
+
+        // Assert
+        Assert.NotNull(dbContext.Players);
+        Assert.Equal(1, dbContext.Players.Count());
+        Assert.Equal(fakePlayer, dbContext.Players.First());
+    }
+
+    [Fact]
+    [Trait("Category", "Integration")]
+    public async Task Update_ExistingPlayer_UpdatesPlayerInDbContextSet()
+    {
+        // Arrange
+        var fakeTeam = Faker.FakeTeam();
+        var fakePlayer = Faker.FakePlayer(active: false, team: fakeTeam);
+        var stubTeamProvider = Mock.Of<ITeamProvider>(x => x.GetBy(fakeTeam.Abbreviation) == fakeTeam);
+
+        await using var connection = await GetDbConnection();
+        await using var dbContext = GetDbContext(connection, stubTeamProvider);
+        await dbContext.Database.MigrateAsync();
+
+        await dbContext.AddAsync(fakePlayer);
+        await dbContext.SaveChangesAsync();
+
+        var repo = new EntityFrameworkPlayerRepository(dbContext);
+
+        // Act
+        fakePlayer.Activate();
+        await repo.Update(fakePlayer);
+        await dbContext.SaveChangesAsync();
+
+        // Assert
+        Assert.Equal(fakePlayer, dbContext.Players.First());
+        Assert.True(fakePlayer.Active);
+    }
+
+    [Fact]
+    [Trait("Category", "Integration")]
+    public async Task GetByMlbId_PlayerMlbId_ReturnsPlayerFromDbContextSet()
+    {
+        // Arrange
+        var fakeTeam = Faker.FakeTeam();
+        var fakePlayer = Faker.FakePlayer(team: fakeTeam);
+        var stubTeamProvider = Mock.Of<ITeamProvider>(x => x.GetBy(fakeTeam.Abbreviation) == fakeTeam);
+
+        await using var connection = await GetDbConnection();
+        await using var dbContext = GetDbContext(connection, stubTeamProvider);
+        await dbContext.Database.MigrateAsync();
+
+        await dbContext.AddAsync(fakePlayer);
+        await dbContext.SaveChangesAsync();
+
+        var repo = new EntityFrameworkPlayerRepository(dbContext);
+
+        // Act
+        var actual = await repo.GetByMlbId(fakePlayer.MlbId);
+
+        // Assert
+        Assert.Equal(fakePlayer, actual);
+    }
+
+    public async Task InitializeAsync() => await _container.StartAsync();
+
+    public async Task DisposeAsync() => await _container.StopAsync();
+
+    private async Task<DbConnection> GetDbConnection()
+    {
+        NpgsqlConnection connection = new(_container.GetConnectionString() + ";Pooling=false;");
+        await connection.OpenAsync();
+        return connection;
+    }
+
+    private PlayersDbContext GetDbContext(DbConnection connection, ITeamProvider stubTeamProvider)
+    {
+        var contextOptions = new DbContextOptionsBuilder<PlayersDbContext>()
+            .UseNpgsql(connection)
+            .Options;
+        return new PlayersDbContext(contextOptions, stubTeamProvider);
+    }
+
+    private sealed class DockerNotRunningException : Exception
+    {
+        public DockerNotRunningException(string? message) : base(message)
+        {
+        }
+    }
+}
---
+++ test/PlayerStatus/com.brettnamba.MlbTheShowForecaster.PlayerStatus.Infrastructure.Tests/TestClasses/Faker.cs
@@ -0,0 +1,50 @@
+﻿using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Common.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Entities;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Enums;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Teams.ValueObjects;
+
+namespace com.brettnamba.MlbTheShowForecaster.PlayerStatus.Infrastructure.Tests.TestClasses;
+
+/// <summary>
+/// Fakes players and teams
+/// </summary>
+public static class Faker
+{
+    public const int DefaultMlbId = 1;
+    public const int DefaultTeamMlbId = 136;
+    public const string DefaultTeamName = "Seattle Mariners";
+    public const string DefaultTeamAbbreviation = "SEA";
+    public const string DefaultFirstName = "First";
+    public const string DefaultLastName = "Last";
+
+    public static Player FakePlayer(int? mlbId = null, Team? team = null, bool active = false)
+    {
+        return Player.Create(
+            mlbId == null ? MlbId.Create(DefaultMlbId) : MlbId.Create(mlbId.Value),
+            PersonName.Create(DefaultFirstName),
+            PersonName.Create(DefaultLastName),
+            new DateTime(1990, 1, 1),
+            Position.Catcher,
+            new DateTime(2010, 1, 1),
+            BatSide.Left,
+            ThrowArm.Left,
+            team,
+            active
+        );
+    }
+
+    public static Team? NoTeam => null;
+
+    public static Team FakeTeam(int? mlbId = DefaultMlbId, string? teamName = DefaultTeamName,
+        string? abbreviation = DefaultTeamAbbreviation)
+    {
+        return Team.Create(
+            mlbId == null ? MlbId.Create(DefaultTeamMlbId) : MlbId.Create(mlbId.Value),
+            teamName == null ? TeamName.Create(DefaultTeamName) : TeamName.Create(teamName),
+            abbreviation == null
+                ? TeamAbbreviation.Create(DefaultTeamAbbreviation)
+                : TeamAbbreviation.Create(abbreviation)
+        );
+    }
+}
