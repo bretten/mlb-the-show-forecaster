0b16f819ee27c02b5ea695481459cfa07adc5338
bretten <brettinbox@gmail.com>
2024-01-13 02:23:43 -0800

    #40 UnitOfWork now publishes domain events

--- test/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests/EntityFramework/TestClasses/TestDbContext.cs
+++ test/com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests/EntityFramework/TestClasses/TestDbContext.cs
@@ -4,5 +4,13 @@ namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.Entity
 
 public class TestDbContext : DbContext
 {
-    public DbSet<TestEntity>? TestEntities { get; set; }
+    public DbSet<TestEntity> TestEntities { get; private init; } = null!;
+
+    protected TestDbContext()
+    {
+    }
+
+    public TestDbContext(DbContextOptions options) : base(options)
+    {
+    }
 }
