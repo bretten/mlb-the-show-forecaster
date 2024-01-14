d388e52ac36609a5a1ff1476364e476d90b74005
bretten <brettinbox@gmail.com>
2024-01-13 23:49:59 -0800

    #37 Player status domain events

---
+++ src/PlayerStatus/com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain/Players/Events/PlayerInactivatedEvent.cs
@@ -0,0 +1,10 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Core.Events;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Common.ValueObjects;
+
+namespace com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Events;
+
+/// <summary>
+/// Published when a Player is inactivated
+/// </summary>
+/// <param name="PlayerMlbId">The MLB ID of the player</param>
+public record PlayerInactivatedEvent(MlbId PlayerMlbId) : IDomainEvent;
---
+++ src/PlayerStatus/com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain/Players/Events/PlayerSignedContractWithTeamEvent.cs
@@ -0,0 +1,11 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Core.Events;
+using com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Common.ValueObjects;
+
+namespace com.brettnamba.MlbTheShowForecaster.PlayerStatus.Domain.Players.Events;
+
+/// <summary>
+/// Published when a Player signs a contract with a Team
+/// </summary>
+/// <param name="PlayerMlbId">The MLB ID of the player</param>
+/// <param name="TeamMlbId">The MLB ID of the team</param>
+public record PlayerSignedContractWithTeamEvent(MlbId PlayerMlbId, MlbId TeamMlbId) : IDomainEvent;
