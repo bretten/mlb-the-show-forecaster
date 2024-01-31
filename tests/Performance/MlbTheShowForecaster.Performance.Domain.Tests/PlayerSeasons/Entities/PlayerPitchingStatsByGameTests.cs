7f7b884721e712ba0d9daa133f6a0201cfaf3604
bretten <brettinbox@gmail.com>
2024-01-30 17:12:10 -0800

    #48 Removing entities for separate PR

--- tests/Performance/MlbTheShowForecaster.Performance.Domain.Tests/PlayerSeasons/Entities/PlayerPitchingStatsByGameTests.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace com.brettnamba.MlbTheShowForecaster.Performance.Domain.Tests.PlayerSeasons.Entities;
-
-public class PlayerPitchingStatsByGameTests
-{
-    [Fact]
-    public void Create_ValidValues_Created()
-    {
-    }
-}
