46b1df41c8bf291eab38314b7df18cd3231e87a0
Brett <bretten@users.noreply.github.com>
2024-02-01 03:59:48 -0800

    #61 Added Earned run average stat value object (#62)

---
+++ src/Performance/MlbTheShowForecaster.Performance.Domain/Statistics/ValueObjects/Pitching/EarnedRunAverage.cs
@@ -0,0 +1,59 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Domain.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.Statistics.ValueObjects.Shared;
+
+namespace com.brettnamba.MlbTheShowForecaster.Performance.Domain.Statistics.ValueObjects.Pitching;
+
+/// <summary>
+/// Earned run average (ERA)
+/// = 9 * (ER / IP)
+///
+/// <para>The average number of earned runs per 9 innings pitched</para>
+/// </summary>
+public sealed class EarnedRunAverage : CalculatedStat
+{
+    /// <summary>
+    /// The number of earned runs allowed
+    /// </summary>
+    public NaturalNumber EarnedRuns { get; }
+
+    /// <summary>
+    /// The number of innings pitched
+    /// </summary>
+    public InningsCount InningsPitched { get; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="earnedRuns">The number of earned runs allowed</param>
+    /// <param name="inningsPitched">The number of innings pitched</param>
+    private EarnedRunAverage(NaturalNumber earnedRuns, InningsCount inningsPitched)
+    {
+        EarnedRuns = earnedRuns;
+        InningsPitched = inningsPitched;
+    }
+
+    /// <summary>
+    /// The number of fractional digits to round the decimal value to
+    /// </summary>
+    protected override int FractionalDigitCount => 2;
+
+    /// <summary>
+    /// Calculates earned run average
+    /// </summary>
+    /// <returns>Earned run average</returns>
+    protected override decimal Calculate()
+    {
+        return 9 * (EarnedRuns.Value / InningsPitched.Value);
+    }
+
+    /// <summary>
+    /// Creates <see cref="EarnedRunAverage"/>
+    /// </summary>
+    /// <param name="earnedRuns">The number of earned runs allowed</param>
+    /// <param name="inningsPitched">The number of innings pitched</param>
+    /// <returns><see cref="EarnedRunAverage"/></returns>
+    public static EarnedRunAverage Create(uint earnedRuns, decimal inningsPitched)
+    {
+        return new EarnedRunAverage(NaturalNumber.Create(earnedRuns), InningsCount.Create(inningsPitched));
+    }
+}
