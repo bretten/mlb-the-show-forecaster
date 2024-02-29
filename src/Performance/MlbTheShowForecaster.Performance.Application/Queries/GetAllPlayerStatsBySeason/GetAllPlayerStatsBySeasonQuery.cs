6ad3fa9ce9da334cb4085dd1f35262317fdf425b
bretten <brettinbox@gmail.com>
2024-02-29 01:43:52 -0800

    #78 Added query to get all PlayerStatsBySeason for a specified season

---
+++ src/Performance/MlbTheShowForecaster.Performance.Application/Queries/GetAllPlayerStatsBySeason/GetAllPlayerStatsBySeasonQuery.cs
@@ -0,0 +1,11 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Application.Cqrs;
+using com.brettnamba.MlbTheShowForecaster.Common.Domain.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.Entities;
+
+namespace com.brettnamba.MlbTheShowForecaster.Performance.Application.Queries.GetAllPlayerStatsBySeason;
+
+/// <summary>
+/// Query that retrieves all <see cref="PlayerStatsBySeason"/> for a specified season
+/// </summary>
+/// <param name="SeasonYear">The season to get <see cref="PlayerStatsBySeason"/> for</param>
+internal readonly record struct GetAllPlayerStatsBySeasonQuery(SeasonYear SeasonYear) : IQuery<IEnumerable<PlayerStatsBySeason>>;
