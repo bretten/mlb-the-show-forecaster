5d57051a960ffc402a76866689683181a02cdcfb
bretten <brettinbox@gmail.com>
2024-03-01 02:40:51 -0800

    #79 Added scorekeeping service to log new games and assess player performance

---
+++ src/Performance/MlbTheShowForecaster.Performance.Domain/PlayerSeasons/Services/PlayerSeasonScorekeeper.cs
@@ -0,0 +1,133 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PerformanceAssessment.Services;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.Entities;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.ValueObjects;
+
+namespace com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.Services;
+
+/// <summary>
+/// Service that will score a player's season -- it will log new games for the season and assess the player's
+/// performance to date
+/// </summary>
+public sealed class PlayerSeasonScorekeeper : IPlayerSeasonScorekeeper
+{
+    /// <summary>
+    /// Performance assessment requirements
+    /// </summary>
+    private readonly IPerformanceAssessmentRequirements _performanceAssessmentRequirements;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="performanceAssessmentRequirements">Performance assessment requirements</param>
+    public PlayerSeasonScorekeeper(IPerformanceAssessmentRequirements performanceAssessmentRequirements)
+    {
+        _performanceAssessmentRequirements = performanceAssessmentRequirements;
+    }
+
+    /// <summary>
+    /// Scores a player's season by logging new games and assessing the player's performance
+    /// </summary>
+    /// <param name="playerStatsBySeason">The player's season stats</param>
+    /// <param name="performanceComparisonDate">The date used in the player's performance assessment</param>
+    /// <param name="playerBattingStatsByGamesToDate">The player's batting stats by games to date</param>
+    /// <param name="playerPitchingStatsByGamesToDate">The player's pitching stats by games to date</param>
+    /// <param name="playerFieldingStatsByGamesToDate">The player's fielding stats by games to date</param>
+    /// <returns>The updated <see cref="PlayerStatsBySeason"/></returns>
+    public PlayerStatsBySeason ScoreSeason(PlayerStatsBySeason playerStatsBySeason, DateTime performanceComparisonDate,
+        IEnumerable<PlayerBattingStatsByGame> playerBattingStatsByGamesToDate,
+        IEnumerable<PlayerPitchingStatsByGame> playerPitchingStatsByGamesToDate,
+        IEnumerable<PlayerFieldingStatsByGame> playerFieldingStatsByGamesToDate)
+    {
+        ScoreBatting(ref playerStatsBySeason, performanceComparisonDate, playerBattingStatsByGamesToDate);
+        ScorePitching(ref playerStatsBySeason, performanceComparisonDate, playerPitchingStatsByGamesToDate);
+        ScoreFielding(ref playerStatsBySeason, performanceComparisonDate, playerFieldingStatsByGamesToDate);
+        return playerStatsBySeason;
+    }
+
+    /// <summary>
+    /// Logs new batting games
+    /// </summary>
+    /// <param name="playerStatsBySeason">The player season stats as it exists in the system currently</param>
+    /// <param name="performanceComparisonDate">The date used in the player's performance assessment</param>
+    /// <param name="statsByGamesToDate">The most up-to-date player season stats from the external MLB source</param>
+    private void ScoreBatting(ref PlayerStatsBySeason playerStatsBySeason, DateTime performanceComparisonDate,
+        IEnumerable<PlayerBattingStatsByGame> statsByGamesToDate)
+    {
+        // The stats by games before the most recent update
+        var previousStatsByGames = playerStatsBySeason.BattingStatsByGamesChronologically;
+
+        // Get stats by games that have not yet been recorded by the scorekeeper
+        var newStatsByGames = statsByGamesToDate.Except(previousStatsByGames).ToList();
+        if (!newStatsByGames.Any())
+        {
+            return;
+        }
+
+        // Log new games
+        foreach (var statsByGame in newStatsByGames)
+        {
+            playerStatsBySeason.LogBattingGame(statsByGame);
+        }
+
+        // Assess performance to date
+        playerStatsBySeason.AssessBattingPerformance(performanceComparisonDate, _performanceAssessmentRequirements);
+    }
+
+    /// <summary>
+    /// Logs new pitching games
+    /// </summary>
+    /// <param name="playerStatsBySeason">The player season stats as it exists in the system currently</param>
+    /// <param name="performanceComparisonDate">The date used in the player's performance assessment</param>
+    /// <param name="statsByGamesToDate">The most up-to-date player season stats from the external MLB source</param>
+    private void ScorePitching(ref PlayerStatsBySeason playerStatsBySeason, DateTime performanceComparisonDate,
+        IEnumerable<PlayerPitchingStatsByGame> statsByGamesToDate)
+    {
+        // The stats by games before the most recent update
+        var previousStatsByGames = playerStatsBySeason.PitchingStatsByGamesChronologically;
+
+        // Get stats by games that have not yet been recorded by the scorekeeper
+        var newStatsByGames = statsByGamesToDate.Except(previousStatsByGames).ToList();
+        if (!newStatsByGames.Any())
+        {
+            return;
+        }
+
+        // Log new games
+        foreach (var statsByGame in newStatsByGames)
+        {
+            playerStatsBySeason.LogPitchingGame(statsByGame);
+        }
+
+        // Assess performance to date
+        playerStatsBySeason.AssessPitchingPerformance(performanceComparisonDate, _performanceAssessmentRequirements);
+    }
+
+    /// <summary>
+    /// Logs new fielding games
+    /// </summary>
+    /// <param name="playerStatsBySeason">The player season stats as it exists in the system currently</param>
+    /// <param name="performanceComparisonDate">The date used in the player's performance assessment</param>
+    /// <param name="statsByGamesToDate">The most up-to-date player season stats from the external MLB source</param>
+    private void ScoreFielding(ref PlayerStatsBySeason playerStatsBySeason, DateTime performanceComparisonDate,
+        IEnumerable<PlayerFieldingStatsByGame> statsByGamesToDate)
+    {
+        // The stats by games before the most recent update
+        var previousStatsByGames = playerStatsBySeason.FieldingStatsByGamesChronologically;
+
+        // Get stats by games that have not yet been recorded by the scorekeeper
+        var newStatsByGames = statsByGamesToDate.Except(previousStatsByGames).ToList();
+        if (!newStatsByGames.Any())
+        {
+            return;
+        }
+
+        // Log new games
+        foreach (var statsByGame in newStatsByGames)
+        {
+            playerStatsBySeason.LogFieldingGame(statsByGame);
+        }
+
+        // Assess performance to date
+        playerStatsBySeason.AssessFieldingPerformance(performanceComparisonDate, _performanceAssessmentRequirements);
+    }
+}
---
+++ tests/Performance/MlbTheShowForecaster.Performance.Domain.Tests/PlayerSeasons/Services/PlayerSeasonScorekeeperTests.cs
@@ -0,0 +1,93 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Domain.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.Events.Participation;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PerformanceAssessment.Services;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.Services;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.Statistics.ValueObjects.Shared;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.Tests.PlayerSeasons.TestClasses;
+using Moq;
+
+namespace com.brettnamba.MlbTheShowForecaster.Performance.Domain.Tests.PlayerSeasons.Services;
+
+public class PlayerSeasonScorekeeperTests
+{
+    [Fact]
+    public void ScoreSeason_NewStatsByGames_ReturnsUpdatedPlayerStatsBySeason()
+    {
+        /*
+         * Arrange
+         */
+
+        // The entity in the system only has games scored on 3/31
+        var fakePlayerStatsBySeason = Faker.FakePlayerSeasonStats(
+            battingStatsByGames: new List<PlayerBattingStatsByGame>()
+                { Faker.FakePlayerBattingStats(gameDate: new DateTime(2024, 3, 31)) },
+            pitchingStatsByGames: new List<PlayerPitchingStatsByGame>()
+                { Faker.FakePlayerPitchingStats(gameDate: new DateTime(2024, 3, 31)) },
+            fieldingStatsByGames: new List<PlayerFieldingStatsByGame>()
+                { Faker.FakePlayerFieldingStats(gameDate: new DateTime(2024, 3, 31)) }
+        );
+
+        // The stats by games to date, some of which have not yet been scored yet
+        var fakeBattingStatsByGameToDate = new List<PlayerBattingStatsByGame>()
+        {
+            Faker.FakePlayerBattingStats(gameDate: new DateTime(2024, 3, 31)),
+            Faker.FakePlayerBattingStats(gameDate: new DateTime(2024, 4, 1))
+        };
+        var fakePitchingStatsByGameToDate = new List<PlayerPitchingStatsByGame>()
+        {
+            Faker.FakePlayerPitchingStats(gameDate: new DateTime(2024, 3, 31)),
+            Faker.FakePlayerPitchingStats(gameDate: new DateTime(2024, 4, 1))
+        };
+        var fakeFieldingStatsByGameToDate = new List<PlayerFieldingStatsByGame>()
+        {
+            Faker.FakePlayerFieldingStats(gameDate: new DateTime(2024, 3, 31)),
+            Faker.FakePlayerFieldingStats(gameDate: new DateTime(2024, 4, 1))
+        };
+
+        // Performance assessment
+        var mockPerformanceAssessmentRequirements = Mock.Of<IPerformanceAssessmentRequirements>();
+        var comparisonDate = new DateTime(2024, 4, 1);
+
+        // Scorekeeper
+        var scorekeeper = new PlayerSeasonScorekeeper(mockPerformanceAssessmentRequirements);
+
+        /*
+         * Act
+         */
+        var actual = scorekeeper.ScoreSeason(fakePlayerStatsBySeason, comparisonDate,
+            fakeBattingStatsByGameToDate, fakePitchingStatsByGameToDate, fakeFieldingStatsByGameToDate);
+
+        /*
+         * Assert
+         */
+        // Was the entity updated?
+        Assert.Equal(fakeBattingStatsByGameToDate.OrderBy(x => x.GameDate),
+            actual.BattingStatsByGamesChronologically);
+        Assert.Equal(fakePitchingStatsByGameToDate.OrderBy(x => x.GameDate),
+            actual.PitchingStatsByGamesChronologically);
+        Assert.Equal(fakeFieldingStatsByGameToDate.OrderBy(x => x.GameDate),
+            actual.FieldingStatsByGamesChronologically);
+
+        // Were the new games logged?
+        Assert.Equal(3, actual.DomainEvents.Count);
+        Assert.Equal(1,
+            actual.DomainEvents.Count(x =>
+                x is PlayerBattedInGameEvent gameEvent && gameEvent.Date == new DateTime(2024, 4, 1)));
+        Assert.Equal(1,
+            actual.DomainEvents.Count(x =>
+                x is PlayerPitchedInGameEvent gameEvent && gameEvent.Date == new DateTime(2024, 4, 1)));
+        Assert.Equal(1,
+            actual.DomainEvents.Count(x =>
+                x is PlayerFieldedInGameEvent gameEvent && gameEvent.Date == new DateTime(2024, 4, 1)));
+
+        // Was the player's performance assessed?
+        Mock.Get(mockPerformanceAssessmentRequirements)
+            .Verify(x => x.AreBattingAssessmentRequirementsMet(It.IsAny<NaturalNumber>()), Times.Once);
+        Mock.Get(mockPerformanceAssessmentRequirements).Verify(
+            x => x.ArePitchingAssessmentRequirementsMet(It.IsAny<InningsCount>(), It.IsAny<NaturalNumber>()),
+            Times.Once);
+        Mock.Get(mockPerformanceAssessmentRequirements)
+            .Verify(x => x.AreFieldingAssessmentRequirementsMet(It.IsAny<NaturalNumber>()), Times.Once);
+    }
+}
