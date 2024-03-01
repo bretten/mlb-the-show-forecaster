1e22def7c9f1233b3ed38962f5d1c3bda723e9a4
bretten <brettinbox@gmail.com>
2024-03-01 02:55:15 -0800

    #81 Defined Player stats contract

---
+++ src/Performance/MlbTheShowForecaster.Performance.Application/Services/IPlayerStats.cs
@@ -0,0 +1,17 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Domain.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.Performance.Application.Dtos;
+
+namespace com.brettnamba.MlbTheShowForecaster.Performance.Application.Services;
+
+/// <summary>
+/// Defines a service that will provide a player's season stats
+/// </summary>
+public interface IPlayerStats
+{
+    /// <summary>
+    /// Gets a player's season stats
+    /// </summary>
+    /// <param name="playerMlbId">The MLB ID of the player</param>
+    /// <returns>The player's season stats</returns>
+    Task<PlayerSeason> GetPlayerSeason(MlbId playerMlbId);
+}
