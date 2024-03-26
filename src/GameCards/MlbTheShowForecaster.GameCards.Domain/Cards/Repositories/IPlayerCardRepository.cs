c3613f9462df7bebe84208a558290057c33e4746
bretten <brettinbox@gmail.com>
2024-03-26 00:57:04 -0700

    #111 GameCards repository contracts

---
+++ src/GameCards/MlbTheShowForecaster.GameCards.Domain/Cards/Repositories/IPlayerCardRepository.cs
@@ -0,0 +1,39 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Domain.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.GameCards.Domain.Cards.Entities;
+using com.brettnamba.MlbTheShowForecaster.GameCards.Domain.Cards.ValueObjects;
+
+namespace com.brettnamba.MlbTheShowForecaster.GameCards.Domain.Cards.Repositories;
+
+/// <summary>
+/// Defines a repository for <see cref="PlayerCard"/>
+/// </summary>
+public interface IPlayerCardRepository
+{
+    /// <summary>
+    /// Should add a <see cref="PlayerCard"/>
+    /// </summary>
+    /// <param name="card">The <see cref="PlayerCard"/> to add</param>
+    /// <returns>The completed task</returns>
+    Task Add(PlayerCard card);
+
+    /// <summary>
+    /// Should update a <see cref="PlayerCard"/>
+    /// </summary>
+    /// <param name="card">The <see cref="PlayerCard"/> to update</param>
+    /// <returns>The completed task</returns>
+    Task Update(PlayerCard card);
+
+    /// <summary>
+    /// Should return all <see cref="PlayerCard"/>s for a specific season
+    /// </summary>
+    /// <param name="season">The season to retrieve <see cref="PlayerCard"/>s for</param>
+    /// <returns>The collection of <see cref="PlayerCard"/>s for the season</returns>
+    Task<IEnumerable<PlayerCard>> GetAll(SeasonYear season);
+
+    /// <summary>
+    /// Should return a <see cref="PlayerCard"/> for the specified <see cref="CardExternalId"/>
+    /// </summary>
+    /// <param name="externalId">The <see cref="CardExternalId"/> of the <see cref="PlayerCard"/></param>
+    /// <returns>The corresponding <see cref="PlayerCard"/></returns>
+    Task<PlayerCard> GetByExternalId(CardExternalId externalId);
+}
---
+++ src/GameCards/MlbTheShowForecaster.GameCards.Domain/Marketplace/Repositories/IListingRepository.cs
@@ -0,0 +1,31 @@
+﻿using com.brettnamba.MlbTheShowForecaster.GameCards.Domain.Cards.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.GameCards.Domain.Marketplace.Entities;
+
+namespace com.brettnamba.MlbTheShowForecaster.GameCards.Domain.Marketplace.Repositories;
+
+/// <summary>
+/// Defines a repository for <see cref="Listing"/>
+/// </summary>
+public interface IListingRepository
+{
+    /// <summary>
+    /// Should add a <see cref="Listing"/>
+    /// </summary>
+    /// <param name="listing">The <see cref="Listing"/> to add</param>
+    /// <returns>The completed task</returns>
+    Task Add(Listing listing);
+
+    /// <summary>
+    /// Should update a <see cref="Listing"/>
+    /// </summary>
+    /// <param name="listing">The <see cref="Listing"/> to update</param>
+    /// <returns>The completed task</returns>
+    Task Update(Listing listing);
+
+    /// <summary>
+    /// Should return a <see cref="Listing"/> for the specified <see cref="CardExternalId"/>
+    /// </summary>
+    /// <param name="externalId">The <see cref="CardExternalId"/> of the <see cref="Listing"/></param>
+    /// <returns>The corresponding <see cref="Listing"/></returns>
+    Task<Listing> GetByExternalId(CardExternalId externalId);
+}
