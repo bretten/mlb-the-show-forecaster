2f58518c64e2d3113f87afe2a4fbd145a373a087
bretten <brettinbox@gmail.com>
2024-03-26 03:08:58 -0700

    #121 MlbTheShowApi now has factory that can create client by year

---
+++ src/ExternalApis/MlbTheShowForecaster.ExternalApis.MlbTheShowApi/Dtos/Enums/Year.cs
@@ -0,0 +1,12 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.ExternalApis.MlbTheShowApi.Dtos.Enums;
+
+/// <summary>
+/// Represents the supported years for the MLB The Show API
+/// </summary>
+public enum Year
+{
+    Season2021 = 2021,
+    Season2022 = 2022,
+    Season2023 = 2023,
+    Season2024 = 2024
+}
