00c66d68af3ffc8217038b4d102ef1a934408aa2
bretten <brettinbox@gmail.com>
2024-04-10 03:18:58 -0700

    #144 Cards Design time db context factory

---
+++ src/GameCards/MlbTheShowForecaster.GameCards.Infrastructure/Cards/EntityFrameworkCore/CardsDesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace com.brettnamba.MlbTheShowForecaster.GameCards.Infrastructure.Cards.EntityFrameworkCore;
+
+/// <summary>
+/// Design-time configuration for the DB context. Only used when using tools like dotnet ef migrations
+/// and never used during application run-time
+/// </summary>
+[ExcludeFromCodeCoverage]
+public sealed class CardsDesignTimeDbContextFactory : IDesignTimeDbContextFactory<CardsDbContext>
+{
+    /// <summary>
+    /// Creates the DB context
+    /// </summary>
+    /// <param name="args">Command-line args</param>
+    /// <returns>The DB context</returns>
+    public CardsDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CardsDbContext>();
+        optionsBuilder.UseNpgsql();
+        return new CardsDbContext(optionsBuilder.Options);
+    }
+}
