246597cfcb877167b7b51e4c5d080fa0045ac590
bretten <brettinbox@gmail.com>
2024-04-11 13:00:38 -0700

    #145 Excluding EF Core Migrations from code coverage

--- src/Performance/MlbTheShowForecaster.Performance.Infrastructure/PlayerSeasons/EntityFrameworkCore/Migrations/20240312090341_InitialCreatePlayerStatsBySeason.cs
+++ src/Performance/MlbTheShowForecaster.Performance.Infrastructure/PlayerSeasons/EntityFrameworkCore/Migrations/20240312090341_InitialCreatePlayerStatsBySeason.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,6 +7,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace com.brettnamba.MlbTheShowForecaster.Performance.Infrastructure.PlayerSeasons.EntityFrameworkCore.Migrations
 {
     /// <inheritdoc />
+    [ExcludeFromCodeCoverage]
     public partial class InitialCreatePlayerStatsBySeason : Migration
     {
         /// <inheritdoc />
