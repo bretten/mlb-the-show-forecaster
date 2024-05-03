c5a245b2a8f9421d088266fa270c06466cc43dbf
bretten <brettinbox@gmail.com>
2024-05-03 16:58:09 -0700

    #166 Added hosts

--- src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/Program.cs
+++ src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddLogging();
+    })
+    .Build();
+
+await host.StartAsync();
+
+Console.ReadLine();
