ea49063985242e072be1a013c9a8d391ea5300e7
Brett <bretten@users.noreply.github.com>
2024-05-03 00:16:22 -0700

    #165 Added library that handles the execution and lifecycle of an app (#169)
    * #165 Added library that handles the execution and lifecycle of an app
    
    * 165 Dispose test

---
+++ src/Common/Execution/MlbTheShowForecaster.Common.Execution.Host/Services/NestedBackgroundService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Execution.Host.Services;
+
+/// <summary>
+/// <see cref="BackgroundService"/> that hosts a nested, underlying service <see cref="T"/> so that it can do work
+/// throughout the application's lifetime
+/// </summary>
+/// <typeparam name="T">The nested, underlying service that will run for the lifetime of the application</typeparam>
+public abstract class NestedBackgroundService<T> : BackgroundService where T : IDisposable
+{
+    /// <summary>
+    /// The nested, underlying service that will run for the lifetime of the application
+    /// </summary>
+    protected readonly T Service;
+
+    /// <summary>
+    /// True if the nested service has already been disposed
+    /// </summary>
+    protected bool IsNestedServiceDisposed;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="service">The nested, underlying service that will run for the lifetime of the application</param>
+    protected NestedBackgroundService(T service)
+    {
+        Service = service;
+    }
+
+    /// <summary>
+    /// Registers clean up duties on the <see cref="stoppingToken"/>. Inheriting classes can override and reference
+    /// the base method to automatically setup clean up
+    /// </summary>
+    /// <param name="stoppingToken"><inheritdoc /></param>
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // Make sure cleanup is either down via Dispose or the CancellationToken
+        stoppingToken.Register(CleanUpNestedService);
+
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Disposes of the <see cref="BackgroundService"/> and the nested service <see cref="T"/>
+    /// </summary>
+    public override void Dispose()
+    {
+        base.Dispose();
+        // Clean up the nested service if it has not already been disposed
+        if (!IsNestedServiceDisposed)
+        {
+            CleanUpNestedService();
+        }
+    }
+
+    /// <summary>
+    /// Disposes of the nested service
+    /// </summary>
+    private void CleanUpNestedService()
+    {
+        IsNestedServiceDisposed = true;
+        Service.Dispose();
+    }
+}
---
+++ src/Common/Execution/MlbTheShowForecaster.Common.Execution.Host/Services/ScheduledBackgroundService.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Hosting;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Execution.Host.Services;
+
+/// <summary>
+/// A <see cref="BackgroundService"/> that will run the containing service on the specified time interval for the entirety
+/// of the application's lifetime
+/// <para>The <see cref="BackgroundService"/> will run an action on <see cref="T"/> and will be stopped when the host is shut down</para>
+/// </summary>
+/// <typeparam name="T"><inheritdoc /></typeparam>
+public sealed class ScheduledBackgroundService<T> : NestedBackgroundService<T> where T : IDisposable
+{
+    /// <summary>
+    /// The work that the service will do on an interval
+    /// </summary>
+    private readonly Func<T, Task> _action;
+
+    /// <summary>
+    /// The interval to run the nested service's work on
+    /// </summary>
+    private readonly TimeSpan _interval;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="service"><inheritdoc /></param>
+    /// <param name="action">The work that the service will do on an interval</param>
+    /// <param name="interval">The interval to run the nested service's work on</param>
+    public ScheduledBackgroundService(T service, Func<T, Task> action, TimeSpan interval) : base(service)
+    {
+        _action = action;
+        _interval = interval;
+    }
+
+    /// <summary>
+    /// Runs the nested service's work on the specified interval until the host is stopped (<see cref="IHostedService.StopAsync(CancellationToken)"/>)
+    /// </summary>
+    /// <param name="stoppingToken"><inheritdoc cref="BackgroundService.ExecuteAsync" path="/param[@name='stoppingToken']"/></param>
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        // The base ExecuteAsync will ensure cleanup is done for the nested service
+        await base.ExecuteAsync(stoppingToken);
+
+        // Timer for the nested service
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        // Check the timer and run the nested service on an interval
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (stopwatch.ElapsedMilliseconds <= _interval.TotalMilliseconds) continue;
+
+            // Get the task that the nested service will perform
+            var task = _action.Invoke(Service);
+            // If the task has already been completed, force the method to complete asynchronously and return to the caller
+            if (task.GetAwaiter().IsCompleted)
+            {
+                await Task.Yield();
+            }
+
+            // The task is not complete yet, so execute it
+            await task;
+
+            // Restart the timer now that the nested service has executed
+            stopwatch.Restart();
+        }
+    }
+}
