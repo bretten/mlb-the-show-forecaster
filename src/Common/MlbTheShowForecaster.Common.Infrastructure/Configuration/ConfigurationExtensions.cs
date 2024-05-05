1f5e2b1a6c6538986f24fc8f862d78c13d3f435f
Brett <bretten@users.noreply.github.com>
2024-05-04 18:25:03 -0700

    #180 Added IConfiguration extensions that require a value to be set (#181)

---
+++ src/Common/MlbTheShowForecaster.Common.Infrastructure/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,61 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Configuration.Exceptions;
+using Microsoft.Extensions.Configuration;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Configuration;
+
+/// <summary>
+/// Extensions for <see cref="IConfiguration"/>
+/// </summary>
+public static class ConfigurationExtensions
+{
+    /// <summary>
+    /// Requires that the key and value be set when attempting to get the value, otherwise an exception of type
+    /// <see cref="ConfigurationNotSetException"/> will be thrown
+    /// </summary>
+    /// <param name="configuration">The current <see cref="IConfiguration"/></param>
+    /// <param name="key">The key of the value</param>
+    /// <typeparam name="T">The type of the value</typeparam>
+    /// <returns>The value</returns>
+    /// <exception cref="ConfigurationNotSetException">Thrown if the key or value is not set</exception>
+    public static T GetRequiredValue<T>(this IConfiguration configuration, string key)
+    {
+        var section = configuration.GetSection(key);
+        if (!section.Exists())
+        {
+            throw new ConfigurationNotSetException($"ConfigurationSection does not exist for key {key}");
+        }
+
+        var value = section.Get<T>();
+        if (value == null || (value is string s && string.IsNullOrWhiteSpace(s)))
+        {
+            throw new ConfigurationNotSetException($"Configuration value not set for key {key}");
+        }
+
+        return value;
+    }
+
+    /// <summary>
+    /// Requires that the key and connection string be set, otherwise an exception of type
+    /// <see cref="ConfigurationNotSetException"/> will be thrown
+    /// </summary>
+    /// <param name="configuration">The current <see cref="IConfiguration"/></param>
+    /// <param name="key">The key for the "ConnectionStrings" section</param>
+    /// <returns>Connection string</returns>
+    /// <exception cref="ConfigurationNotSetException">Thrown if the key or connection string is not set</exception>
+    public static string GetRequiredConnectionString(this IConfiguration configuration, string key)
+    {
+        var section = configuration.GetSection("ConnectionStrings");
+        if (!section.Exists())
+        {
+            throw new ConfigurationNotSetException("ConnectionStrings section does not exist");
+        }
+
+        var value = configuration.GetConnectionString(key);
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new ConfigurationNotSetException($"ConnectionString not set for key {key}");
+        }
+
+        return value;
+    }
+}
---
+++ tests/Common/MlbTheShowForecaster.Common.Infrastructure.Tests/Configuration/ConfigurationExtensionsTests.cs
@@ -0,0 +1,177 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Configuration;
+using com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Configuration.Exceptions;
+using Microsoft.Extensions.Configuration;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Tests.Configuration;
+
+public class ConfigurationExtensionsTests
+{
+    [Fact]
+    public void GetRequiredValue_KeyDoesNotExist_ThrowsException()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" }
+        };
+        var config = GetConfig(settings);
+
+        var action = () => config.GetRequiredValue<string>("Key3");
+
+        // Act
+        var actual = Record.Exception(action);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<ConfigurationNotSetException>(actual);
+    }
+
+    [Fact]
+    public void GetRequiredValue_ValueDoesNotExist_ThrowsException()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+            { "Key3", null },
+        };
+        var config = GetConfig(settings);
+
+        var action = () => config.GetRequiredValue<string>("Key3");
+
+        // Act
+        var actual = Record.Exception(action);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<ConfigurationNotSetException>(actual);
+    }
+
+    [Fact]
+    public void GetRequiredValue_KeyAndStringValueExist_ReturnsValue()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+            { "Key3", "Value3" },
+        };
+        var config = GetConfig(settings);
+
+        // Act
+        var actual = config.GetRequiredValue<string>("Key3");
+
+        // Assert
+        Assert.Equal("Value3", actual);
+    }
+
+    [Fact]
+    public void GetRequiredValue_KeyAndIntValueExist_ReturnsValue()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+            { "Key3", "3" },
+        };
+        var config = GetConfig(settings);
+
+        // Act
+        var actual = config.GetRequiredValue<int>("Key3");
+
+        // Assert
+        Assert.Equal(3, actual);
+    }
+
+    [Fact]
+    public void GetRequiredValue_KeyAndDecimalValueExist_ReturnsValue()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+            { "Key3", "3.14" },
+        };
+        var config = GetConfig(settings);
+
+        // Act
+        var actual = config.GetRequiredValue<decimal>("Key3");
+
+        // Assert
+        Assert.Equal(3.14m, actual);
+    }
+
+    [Fact]
+    public void GetRequiredConnectionString_KeyDoesNotExist_ThrowsException()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "ConnectionStrings:Key1", "Conn1" },
+            { "ConnectionStrings:Key2", "Conn2" }
+        };
+        var config = GetConfig(settings);
+
+        var action = () => config.GetRequiredConnectionString("Key3");
+
+        // Act
+        var actual = Record.Exception(action);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<ConfigurationNotSetException>(actual);
+    }
+
+    [Fact]
+    public void GetRequiredConnectionString_ConnectionStringDoesNotExist_ThrowsException()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "ConnectionStrings:Key1", "Conn1" },
+            { "ConnectionStrings:Key2", "Conn2" },
+            { "ConnectionStrings:Key3", null }
+        };
+        var config = GetConfig(settings);
+
+        var action = () => config.GetRequiredConnectionString("Key3");
+
+        // Act
+        var actual = Record.Exception(action);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<ConfigurationNotSetException>(actual);
+    }
+
+    [Fact]
+    public void GetRequiredConnectionString_KeyAndConnectionStringExist_ReturnsValue()
+    {
+        // Arrange
+        var settings = new Dictionary<string, string?>
+        {
+            { "ConnectionStrings:Key1", "Conn1" },
+            { "ConnectionStrings:Key2", "Conn2" },
+            { "ConnectionStrings:Key3", "Conn3" }
+        };
+        var config = GetConfig(settings);
+
+        // Act
+        var actual = config.GetRequiredConnectionString("Key3");
+
+        // Assert
+        Assert.Equal("Conn3", actual);
+    }
+
+    private static IConfiguration GetConfig(Dictionary<string, string?> settings)
+    {
+        return new ConfigurationBuilder()
+            .AddInMemoryCollection(settings)
+            .Build();
+    }
+}
