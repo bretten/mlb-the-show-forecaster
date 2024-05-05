006419c31b56117044bd0c285d716ffe7d46bec5
bretten <brettinbox@gmail.com>
2024-05-04 18:16:32 -0700

    #180 Added IConfiguration extensions that require a value to be set

---
+++ src/Common/MlbTheShowForecaster.Common.Infrastructure/Configuration/Exceptions/ConfigurationNotSetException.cs
@@ -0,0 +1,12 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Infrastructure.Configuration.Exceptions;
+
+/// <summary>
+/// Thrown when <see cref="ConfigurationExtensions.GetRequiredValue{T}"/> cannot find the specified key or value for
+/// that key
+/// </summary>
+public sealed class ConfigurationNotSetException : Exception
+{
+    public ConfigurationNotSetException(string? message) : base(message)
+    {
+    }
+}
