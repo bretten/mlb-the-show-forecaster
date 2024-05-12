273b45947460b826929161d3fb7d427423c00374
bretten <brettinbox@gmail.com>
2024-05-11 18:10:02 -0700

    #213 Added calendar abstraction and replaced calls to DateTime.Now

---
+++ tests/Common/MlbTheShowForecaster.Common.Tests/DateAndTime/CalendarTests.cs
@@ -0,0 +1,19 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.DateAndTime;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Tests.DateAndTime;
+
+public class CalendarTests
+{
+    [Fact]
+    public void Today_NoParameters_ReturnsToday()
+    {
+        // Arrange
+        var calendar = new Calendar();
+
+        // Act
+        var actual = calendar.Today();
+
+        // Assert
+        Assert.Equal(DateOnly.FromDateTime(DateTime.Today), actual);
+    }
+}
