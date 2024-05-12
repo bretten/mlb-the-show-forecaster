d00bc9938d7f8f88eae55360aa8ca9ed8f524899
Brett <bretten@users.noreply.github.com>
2024-05-11 18:12:26 -0700

    #213 Added calendar abstraction and replaced calls to DateTime.Now (#215)

---
+++ src/Common/MlbTheShowForecaster.Common/DateAndTime/Calendar.cs
@@ -0,0 +1,13 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.DateAndTime;
+
+/// <summary>
+/// Calendar that provides dates
+/// </summary>
+public sealed class Calendar : ICalendar
+{
+    /// <inheritdoc />
+    public DateOnly Today()
+    {
+        return DateOnly.FromDateTime(DateTime.Today);
+    }
+}
---
+++ src/Common/MlbTheShowForecaster.Common/DateAndTime/ICalendar.cs
@@ -0,0 +1,13 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.DateAndTime;
+
+/// <summary>
+/// Defines an interface for dates
+/// </summary>
+public interface ICalendar
+{
+    /// <summary>
+    /// Today's date
+    /// </summary>
+    /// <returns>Today's date</returns>
+    DateOnly Today();
+}
