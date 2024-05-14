56b1032bcdbaf734b9b61fcc916cfc4f450c6a5f
Brett <bretten@users.noreply.github.com>
2024-05-13 21:54:49 -0700

    #222 Added clock abstraction (#226)
    * #222 Added clock abstraction
    
    * #222 Updating test for unix
    
    * #222 Updating test for unix
    
    * #222 Switching PST id
    
    * #222 PstNow first creates date time

---
+++ src/Common/MlbTheShowForecaster.Common/DateAndTime/Clock.cs
@@ -0,0 +1,21 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.DateAndTime;
+
+/// <summary>
+/// Clock that provides the date and time
+/// </summary>
+public sealed class Clock : IClock
+{
+    /// <inheritdoc />
+    public DateTimeOffset UtcNow()
+    {
+        return DateTimeOffset.UtcNow;
+    }
+
+    /// <inheritdoc />
+    public DateTimeOffset PstNow()
+    {
+        var tzInfo = TimeZoneInfo.FindSystemTimeZoneById("America/Los_Angeles");
+        var dateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, tzInfo);
+        return new DateTimeOffset(dateTime, TimeSpan.FromHours(-7));
+    }
+}
