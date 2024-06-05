f4673cfea3b1b00e81930e717a50d079e8a348c0
bretten <brettinbox@gmail.com>
2024-06-05 16:20:55 -0700

    #258 Added Performance Score

---
+++ src/Performance/MlbTheShowForecaster.Performance.Domain/PerformanceAssessment/ValueObjects/PerformanceScore.cs
@@ -0,0 +1,41 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Domain.SeedWork;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PerformanceAssessment.ValueObjects.Exceptions;
+
+namespace com.brettnamba.MlbTheShowForecaster.Performance.Domain.PerformanceAssessment.ValueObjects;
+
+/// <summary>
+/// Represents a score from <see cref="IPerformanceAssessor"/>
+/// <para>The value is a normalized score on a scale of [0, 1], with 1 being the best possible score.</para>
+/// </summary>
+public sealed class PerformanceScore : ValueObject
+{
+    /// <summary>
+    /// The underlying score value
+    /// </summary>
+    public decimal Value { get; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="value">The underlying score value</param>
+    /// <exception cref="InvalidPerformanceScoreException">Thrown when the specified value is out of range</exception>
+    private PerformanceScore(decimal value)
+    {
+        if (value < 0 || value > 1)
+        {
+            throw new InvalidPerformanceScoreException($"Invalid {nameof(PerformanceScore)}: {value}");
+        }
+
+        Value = value;
+    }
+
+    /// <summary>
+    /// Creates a <see cref="PerformanceScore"/>
+    /// </summary>
+    /// <param name="score">The score</param>
+    /// <returns><see cref="PerformanceScore"/></returns>
+    public static PerformanceScore Create(decimal score)
+    {
+        return new PerformanceScore(score);
+    }
+}
---
+++ tests/Performance/MlbTheShowForecaster.Performance.Domain.Tests/PerformanceAssessment/ValueObjects/PerformanceScoreTests.cs
@@ -0,0 +1,37 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PerformanceAssessment.ValueObjects;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PerformanceAssessment.ValueObjects.Exceptions;
+
+namespace com.brettnamba.MlbTheShowForecaster.Performance.Domain.Tests.PerformanceAssessment.ValueObjects;
+
+public class PerformanceScoreTests
+{
+    [Theory]
+    [InlineData(-0.01)]
+    [InlineData(1.01)]
+    public void Constructor_InvalidScore_ThrowsException(double scoreAsDouble)
+    {
+        // Arrange
+        var score = (decimal)scoreAsDouble;
+        var action = () => PerformanceScore.Create(score);
+
+        // Act
+        var actual = Record.Exception(action);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<InvalidPerformanceScoreException>(actual);
+    }
+
+    [Fact]
+    public void Value_ValidScore_ReturnsScore()
+    {
+        // Arrange
+        const decimal score = 0.7m;
+
+        // Act
+        var actual = PerformanceScore.Create(score);
+
+        // Assert
+        Assert.Equal(0.7m, actual.Value);
+    }
+}
