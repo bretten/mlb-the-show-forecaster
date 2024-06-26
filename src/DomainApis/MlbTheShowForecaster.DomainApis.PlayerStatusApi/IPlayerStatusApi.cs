46e69eda1440a24d6d404c492d65e03923de02cf
bretten <brettinbox@gmail.com>
2024-06-25 19:08:42 -0700

    #265 Added client for PlayerStatus domain API

---
+++ src/DomainApis/MlbTheShowForecaster.DomainApis.PlayerStatusApi/IPlayerStatusApi.cs
@@ -0,0 +1,19 @@
+﻿using com.brettnamba.MlbTheShowForecaster.DomainApis.PlayerStatusApi.Responses;
+using Refit;
+
+namespace com.brettnamba.MlbTheShowForecaster.DomainApis.PlayerStatusApi;
+
+/// <summary>
+/// Represents a client for the API of the PlayerStatus domain
+/// </summary>
+public interface IPlayerStatusApi
+{
+    /// <summary>
+    /// Finds a player by their name and team
+    /// </summary>
+    /// <param name="name">The player's name</param>
+    /// <param name="team">The player's team</param>
+    /// <returns><see cref="PlayerResponse"/> or 404 if no matching player is found</returns>
+    [Get("/players")]
+    Task<ApiResponse<PlayerResponse>> FindPlayer([Query] string name, [Query] string team);
+}
---
+++ src/DomainApis/MlbTheShowForecaster.DomainApis.PlayerStatusApi/Responses/PlayerResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace com.brettnamba.MlbTheShowForecaster.DomainApis.PlayerStatusApi.Responses;
+
+/// <summary>
+/// Represents a player response
+/// </summary>
+/// <param name="MlbId">The MLB ID of the Player</param>
+/// <param name="FirstName">Player's first name</param>
+/// <param name="LastName">Player's last name</param>
+/// <param name="Position">Player's primary position</param>
+/// <param name="Team">The Player's Team</param>
+public readonly record struct PlayerResponse(
+    [property: JsonPropertyName("mlbId")]
+    int MlbId,
+    [property: JsonPropertyName("firstName")]
+    string FirstName,
+    [property: JsonPropertyName("lastName")]
+    string LastName,
+    [property: JsonPropertyName("position")]
+    string Position,
+    [property: JsonPropertyName("team")]
+    string? Team);
