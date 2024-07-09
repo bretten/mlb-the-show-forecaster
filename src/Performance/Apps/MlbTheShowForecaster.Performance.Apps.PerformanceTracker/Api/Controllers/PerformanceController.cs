d434f4e85ec3eb69389fc90df6027f72a26cc6e0
bretten <brettinbox@gmail.com>
2024-07-09 02:11:02 -0700

    #271 Performance endpoint is now filterable by time period

--- src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/Api/Controllers/PerformanceController.cs
+++ src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/Api/Controllers/PerformanceController.cs
@@ -1,5 +1,6 @@
 ﻿using com.brettnamba.MlbTheShowForecaster.Common.Domain.ValueObjects;
 using com.brettnamba.MlbTheShowForecaster.Performance.Apps.PerformanceTracker.Api.Responses;
+using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PerformanceAssessment.Services;
 using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,33 +18,70 @@ public sealed class PerformanceController : Controller
     private readonly IPlayerStatsBySeasonRepository _playerStatsBySeasonRepository;
 
     /// <summary>
+    /// Assesses performance
+    /// </summary>
+    private readonly IPerformanceAssessor _performanceAssessor;
+
+    /// <summary>
+    /// Assesses participation
+    /// </summary>
+    private readonly IParticipationAssessor _participationAssessor;
+
+    /// <summary>
     /// Constructor
     /// </summary>
     /// <param name="playerStatsBySeasonRepository">Repo for player stats</param>
-    public PerformanceController(IPlayerStatsBySeasonRepository playerStatsBySeasonRepository)
+    /// <param name="performanceAssessor">Assesses performance</param>
+    /// <param name="participationAssessor">Assesses participation</param>
+    public PerformanceController(IPlayerStatsBySeasonRepository playerStatsBySeasonRepository,
+        IPerformanceAssessor performanceAssessor, IParticipationAssessor participationAssessor)
     {
         _playerStatsBySeasonRepository = playerStatsBySeasonRepository;
+        _performanceAssessor = performanceAssessor;
+        _participationAssessor = participationAssessor;
     }
 
     /// <summary>
     /// Finds a player's season performance
     /// </summary>
-    /// <param name="seasonQuery">The season</param>
-    /// <param name="playerMlbIdQuery">The player's MLB ID</param>
+    /// <param name="season">The season</param>
+    /// <param name="playerMlbId">The player's MLB ID</param>
+    /// <param name="start">Start date of the stats</param>
+    /// <param name="end">End date of the stats</param>
     /// <returns>The player's season performance, or returns a 404 status code if not found</returns>
     [HttpGet("performance")]
-    public async Task<ActionResult> FindPlayerSeasonPerformance([FromQuery(Name = "season")] ushort seasonQuery,
-        [FromQuery(Name = "playerMlbId")] int playerMlbIdQuery)
+    public async Task<ActionResult> FindPlayerSeasonPerformance([FromQuery] ushort season, [FromQuery] int playerMlbId,
+        [FromQuery] DateOnly start, [FromQuery] DateOnly end)
     {
-        var season = SeasonYear.Create(seasonQuery);
-        var mlbId = MlbId.Create(playerMlbIdQuery);
+        var seasonYear = SeasonYear.Create(season);
+        var mlbId = MlbId.Create(playerMlbId);
 
-        var playerStatsBySeason = await _playerStatsBySeasonRepository.GetBy(season, mlbId);
+        var playerStatsBySeason = await _playerStatsBySeasonRepository.GetBy(seasonYear, mlbId);
         if (playerStatsBySeason == null)
         {
             return new NotFoundResult();
         }
 
-        return new JsonResult(PlayerSeasonPerformanceResponse.From(playerStatsBySeason));
+        var battingStats = playerStatsBySeason.BattingStatsFor(start, end);
+        var battingScore = _performanceAssessor.AssessBatting(battingStats);
+        var significantBattingParticipation = _participationAssessor.AssessBatting(start, end, battingStats);
+
+        var pitchingStats = playerStatsBySeason.PitchingStatsFor(start, end);
+        var pitchingScore = _performanceAssessor.AssessPitching(pitchingStats);
+        var significantPitchingParticipation = _participationAssessor.AssessPitching(start, end, pitchingStats);
+
+        var fieldingStats = playerStatsBySeason.FieldingStatsFor(start, end);
+        var fieldingScore = _performanceAssessor.AssessFielding(fieldingStats);
+        var significantFieldingParticipation = _participationAssessor.AssessFielding(start, end, fieldingStats);
+
+        return new JsonResult(new PlayerSeasonPerformanceResponse(playerStatsBySeason.SeasonYear.Value,
+            playerStatsBySeason.PlayerMlbId.Value,
+            BattingScore: battingScore.Score.Value,
+            HadSignificantBattingParticipation: significantBattingParticipation,
+            PitchingScore: pitchingScore.Score.Value,
+            HadSignificantPitchingParticipation: significantPitchingParticipation,
+            FieldingScore: fieldingScore.Score.Value,
+            HadSignificantFieldingParticipation: significantFieldingParticipation
+        ));
     }
 }
--- src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/Api/Responses/PlayerSeasonPerformanceResponse.cs
+++ src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/Api/Responses/PlayerSeasonPerformanceResponse.cs
@@ -1,6 +1,4 @@
-﻿using com.brettnamba.MlbTheShowForecaster.Performance.Domain.PlayerSeasons.Entities;
-
-namespace com.brettnamba.MlbTheShowForecaster.Performance.Apps.PerformanceTracker.Api.Responses;
+﻿namespace com.brettnamba.MlbTheShowForecaster.Performance.Apps.PerformanceTracker.Api.Responses;
 
 /// <summary>
 /// Represents a player's season performance
@@ -8,27 +6,17 @@ namespace com.brettnamba.MlbTheShowForecaster.Performance.Apps.PerformanceTracke
 /// <param name="Season">The player's season</param>
 /// <param name="MlbId">The player's MLB ID</param>
 /// <param name="BattingScore">The player's batting performance score</param>
+/// <param name="HadSignificantBattingParticipation">True if the player had significant batting participation</param>
 /// <param name="PitchingScore">The player's pitching performance score</param>
+/// <param name="HadSignificantPitchingParticipation">True if the player had significant pitching participation</param>
 /// <param name="FieldingScore">The player's fielding performance score</param>
+/// <param name="HadSignificantFieldingParticipation">True if the player had significant fielding participation</param>
 public readonly record struct PlayerSeasonPerformanceResponse(
     ushort Season,
     int MlbId,
     decimal BattingScore,
+    bool HadSignificantBattingParticipation,
     decimal PitchingScore,
-    decimal FieldingScore)
-{
-    /// <summary>
-    /// Creates a <see cref="PlayerSeasonPerformanceResponse"/> from a <see cref="PlayerStatsBySeason"/>
-    /// </summary>
-    /// <param name="playerStatsBySeason"><see cref="PlayerStatsBySeason"/></param>
-    /// <returns><see cref="PlayerSeasonPerformanceResponse"/></returns>
-    public static PlayerSeasonPerformanceResponse From(PlayerStatsBySeason playerStatsBySeason)
-    {
-        return new PlayerSeasonPerformanceResponse(playerStatsBySeason.SeasonYear.Value,
-            playerStatsBySeason.PlayerMlbId.Value,
-            BattingScore: playerStatsBySeason.BattingScore.Value,
-            PitchingScore: playerStatsBySeason.PitchingScore.Value,
-            FieldingScore: playerStatsBySeason.FieldingScore.Value
-        );
-    }
-};
+    bool HadSignificantPitchingParticipation,
+    decimal FieldingScore,
+    bool HadSignificantFieldingParticipation);
