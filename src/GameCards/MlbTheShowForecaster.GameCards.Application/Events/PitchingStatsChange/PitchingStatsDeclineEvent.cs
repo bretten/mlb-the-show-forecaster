d4b157a87ebe49a903f5aff5da8eff034a31dbd6
bretten <brettinbox@gmail.com>
2024-08-08 01:05:20 -0700

    #276 Fixing comment

--- src/GameCards/MlbTheShowForecaster.GameCards.Application/Events/PitchingStatsChange/PitchingStatsDeclineEvent.cs
+++ src/GameCards/MlbTheShowForecaster.GameCards.Application/Events/PitchingStatsChange/PitchingStatsDeclineEvent.cs
@@ -4,7 +4,7 @@ using com.brettnamba.MlbTheShowForecaster.GameCards.Domain.Cards.ValueObjects;
 namespace com.brettnamba.MlbTheShowForecaster.GameCards.Application.Events.PitchingStatsChange;
 
 /// <summary>
-/// Represents a decline in an entity's batting stats
+/// Represents a decline in an entity's pitching stats
 /// </summary>
 /// <param name="Year">The season of the stats change</param>
 /// <param name="MlbId">The <see cref="Common.Domain.ValueObjects.MlbId"/> of the entity who had the stats change</param>
