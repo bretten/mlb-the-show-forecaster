3e8baf5f7dbc6ef5e815b2c397567bec93b3c63e
Brett <bretten@users.noreply.github.com>
2024-10-15 19:16:22 -0700

    #332 Added pagination result (#333)

---
+++ src/Common/MlbTheShowForecaster.Common.Application/Pagination/PaginationResult.cs
@@ -0,0 +1,60 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Application.Pagination;
+
+/// <summary>
+/// Represents a paginated result of items of type <see cref="T"/>
+/// </summary>
+public sealed class PaginationResult<T>
+{
+    /// <summary>
+    /// The current page
+    /// </summary>
+    public int Page { get; }
+
+    /// <summary>
+    /// The number of items on the page
+    /// </summary>
+    public int PageSize { get; }
+
+    /// <summary>
+    /// The total number of items across all pages
+    /// </summary>
+    public long TotalItems { get; }
+
+    /// <summary>
+    /// The total number of pages
+    /// </summary>
+    public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / PageSize);
+
+    /// <summary>
+    /// The items on the current page
+    /// </summary>
+    public IEnumerable<T> Items { get; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="page">The current page</param>
+    /// <param name="pageSize">The number of items on the page</param>
+    /// <param name="totalItems">The total number of items across all pages</param>
+    /// <param name="items">The items on the current page</param>
+    private PaginationResult(int page, int pageSize, long totalItems, IEnumerable<T> items)
+    {
+        Page = page;
+        PageSize = pageSize;
+        TotalItems = totalItems;
+        Items = items;
+    }
+
+    /// <summary>
+    /// Creates <see cref="PaginationResult{T}"/>
+    /// </summary>
+    /// <param name="page">The current page</param>
+    /// <param name="pageSize">The number of items on the page</param>
+    /// <param name="totalItems">The total number of items across all pages</param>
+    /// <param name="items">The items on the current page</param>
+    /// <returns><see cref="PaginationResult{T}"/></returns>
+    public static PaginationResult<T> Create(int page, int pageSize, long totalItems, IEnumerable<T> items)
+    {
+        return new PaginationResult<T>(page: page, pageSize: pageSize, totalItems: totalItems, items);
+    }
+}
---
+++ tests/Common/MlbTheShowForecaster.Common.Application.Tests/Pagination/PaginationResultTests.cs
@@ -0,0 +1,26 @@
+﻿using com.brettnamba.MlbTheShowForecaster.Common.Application.Pagination;
+
+namespace com.brettnamba.MlbTheShowForecaster.Common.Application.Tests.Pagination;
+
+public class PaginationResultTests
+{
+    [Fact]
+    public void Create_PageInformation_Created()
+    {
+        // Arrange
+        const int page = 2;
+        const int pageSize = 5;
+        const long totalItems = 20;
+        var items = new List<string>() { "A", "B", "C", "D", "E" };
+
+        // Act
+        var actual = PaginationResult<string>.Create(page, pageSize, totalItems, items);
+
+        // Assert
+        Assert.Equal(2, actual.Page);
+        Assert.Equal(5, actual.PageSize);
+        Assert.Equal(items, actual.Items);
+        Assert.Equal(totalItems, actual.TotalItems);
+        Assert.Equal(4, actual.TotalPages);
+    }
+}
