162dbeaeb20e9d536c38382349223246636f4449
bretten <brettinbox@gmail.com>
2024-10-17 15:21:03 -0700

    #335 Job manager

---
+++ src/Common/MlbTheShowForecaster.Common.Application/Jobs/IJobInput.cs
@@ -0,0 +1,8 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Application.Jobs;
+
+/// <summary>
+/// Defines input to a <see cref="IJob"/>
+/// </summary>
+public interface IJobInput
+{
+}
---
+++ src/Common/MlbTheShowForecaster.Common.Application/Jobs/IJobManager.cs
@@ -0,0 +1,19 @@
+﻿namespace com.brettnamba.MlbTheShowForecaster.Common.Application.Jobs;
+
+/// <summary>
+/// Defines a service that manages <see cref="IJob"/>s
+/// </summary>
+public interface IJobManager
+{
+    /// <summary>
+    /// Runs a <see cref="IJob"/> with the specified <see cref="IJobInput"/>
+    /// </summary>
+    /// <param name="input"><see cref="IJobInput"/></param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete</param>
+    /// <typeparam name="T"><see cref="IJob"/></typeparam>
+    /// <typeparam name="TOut"><see cref="IJobOutput"/></typeparam>
+    /// <returns><see cref="IJobOutput"/></returns>
+    Task<TOut> Run<T, TOut>(IJobInput input, CancellationToken cancellationToken = default)
+        where T : IJob
+        where TOut : IJobOutput;
+}
