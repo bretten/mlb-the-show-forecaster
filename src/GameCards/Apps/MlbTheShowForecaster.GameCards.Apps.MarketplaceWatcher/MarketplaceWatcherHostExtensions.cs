ce8343ec68705a4df936b0a019c6ae5a4533c320
bretten <brettinbox@gmail.com>
2024-10-22 02:13:42 -0700

    #348 Updating job intervals

--- src/GameCards/Apps/MlbTheShowForecaster.GameCards.Apps.MarketplaceWatcher/MarketplaceWatcherHostExtensions.cs
+++ src/GameCards/Apps/MlbTheShowForecaster.GameCards.Apps.MarketplaceWatcher/MarketplaceWatcherHostExtensions.cs
@@ -193,7 +193,7 @@ public static class MarketplaceWatcherHostExtensions
             return new ScopedSingleInstanceJobManager(scopeFactory, jobSchedules, commService, logger);
         });
 
-        var jobManagerInterval = ParseInterval("00:00:01:00");
+        var jobManagerInterval = ParseInterval(context.Configuration.GetRequiredValue<string>("Jobs:Interval"));
 
         services.AddHostedService<ScheduledBackgroundService<IJobManager>>(sp =>
             new ScheduledBackgroundService<IJobManager>(sp.GetRequiredService<IServiceScopeFactory>(), JobManagerWork,
--- src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/PerformanceTrackerHostExtensions.cs
+++ src/Performance/Apps/MlbTheShowForecaster.Performance.Apps.PerformanceTracker/PerformanceTrackerHostExtensions.cs
@@ -131,7 +131,7 @@ public static class PerformanceTrackerHostExtensions
             return new ScopedSingleInstanceJobManager(scopeFactory, jobSchedules, commService, logger);
         });
 
-        var jobManagerInterval = ParseInterval("00:00:01:00");
+        var jobManagerInterval = ParseInterval(context.Configuration.GetRequiredValue<string>("Jobs:Interval"));
 
         services.AddHostedService<ScheduledBackgroundService<IJobManager>>(sp =>
             new ScheduledBackgroundService<IJobManager>(sp.GetRequiredService<IServiceScopeFactory>(), JobManagerWork,
