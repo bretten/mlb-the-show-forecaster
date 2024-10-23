0416fed1d5a7cfca6885c92f714e29389746b59c
Brett <bretten@users.noreply.github.com>
2024-10-23 14:38:02 -0700

    #330 Added API gateway (#346)
    * #330 Added API gateway
    
    * #330 API gateway now has authentication
    
    * #330 Fixing CORS option

---
+++ src/Apps/MlbTheShowForecaster.Apps.Gateway/Ocelot/AwsCognitoClaimsAuthorizer.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+using Ocelot.Authorization;
+using Ocelot.DownstreamRouteFinder.UrlMatcher;
+using Ocelot.Responses;
+
+namespace com.brettnamba.MlbTheShowForecaster.Apps.Gateway.Ocelot;
+
+/// <summary>
+/// https://github.com/ThreeMammals/Ocelot/issues/679#issuecomment-662905004
+///
+/// In the case of AWS cognito, its claim type name is "cognito:group". So use "cognito/" instead and replace to what AWS sends
+/// </summary>
+[ExcludeFromCodeCoverage]
+public sealed class AwsCognitoClaimsAuthorizer : IClaimsAuthorizer
+{
+    /// <summary>
+    /// The default <see cref="ClaimsAuthorizer"/> that is normally injected by Ocelot
+    /// </summary>
+    private readonly ClaimsAuthorizer _claimsAuthorizer;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="claimsAuthorizer">The default <see cref="ClaimsAuthorizer"/> that is normally injected by Ocelot</param>
+    public AwsCognitoClaimsAuthorizer(ClaimsAuthorizer claimsAuthorizer)
+    {
+        _claimsAuthorizer = claimsAuthorizer;
+    }
+
+    /// <inheritdoc />
+    public Response<bool> Authorize(ClaimsPrincipal claimsPrincipal, Dictionary<string, string> routeClaimsRequirement,
+        List<PlaceholderNameAndValue> urlPathPlaceholderNameAndValues)
+    {
+        var updatedClaimsRequirement = new Dictionary<string, string>();
+        foreach (var claim in routeClaimsRequirement)
+        {
+            // If we see the custom AWS cognito claim, replace it with its actual value
+            if (claim.Key.StartsWith("cognito/"))
+            {
+                var key = claim.Key.Replace("cognito/", "cognito:");
+                updatedClaimsRequirement.Add(key, claim.Value);
+            }
+            else
+            {
+                // Otherwise, just add the claim as it was originally defined
+                updatedClaimsRequirement.Add(claim.Key, claim.Value);
+            }
+        }
+
+        return _claimsAuthorizer.Authorize(claimsPrincipal, updatedClaimsRequirement, urlPathPlaceholderNameAndValues);
+    }
+}
---
+++ src/Apps/MlbTheShowForecaster.Apps.Gateway/Ocelot/OcelotExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Ocelot.Authorization;
+
+namespace com.brettnamba.MlbTheShowForecaster.Apps.Gateway.Ocelot;
+
+/// <summary>
+/// Ocelot extensions
+/// </summary>
+public static class OcelotExtensions
+{
+    /// <summary>
+    /// Replaces the default <see cref="IClaimsAuthorizer"/> with <see cref="AwsCognitoClaimsAuthorizer"/>
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/></param>
+    /// <returns>Updated <see cref="IServiceCollection"/></returns>
+    public static IServiceCollection AddAwsCognitoClaimsAuthorizer(this IServiceCollection services)
+    {
+        // Remove the default IClaimsAuthorizer
+        var serviceDescriptor = services.First(x => x.ServiceType == typeof(IClaimsAuthorizer));
+        services.Remove(serviceDescriptor);
+
+        // Replace the original IClaimsAuthorizer with the AWS Cognito version
+        var awsDescriptor = new ServiceDescriptor(serviceDescriptor.ImplementationType!,
+            serviceDescriptor.ImplementationType!, serviceDescriptor.Lifetime);
+        services.Add(awsDescriptor);
+
+        services.AddTransient<IClaimsAuthorizer, AwsCognitoClaimsAuthorizer>();
+
+        return services;
+    }
+}
