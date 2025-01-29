f0ca4b5803df3a3330b8cb5e539bb1980989f3ae
bretten <brettinbox@gmail.com>
2025-01-29 15:36:28 -0800

    #452 Build is now included in logs

--- src/GameCards/Apps/MlbTheShowForecaster.GameCards.Apps.MarketplaceWatcher/Program.cs
+++ src/GameCards/Apps/MlbTheShowForecaster.GameCards.Apps.MarketplaceWatcher/Program.cs
@@ -1,6 +1,9 @@
-﻿using com.brettnamba.MlbTheShowForecaster.GameCards.Apps.MarketplaceWatcher;
+﻿using System.Diagnostics;
+using System.Reflection;
+using com.brettnamba.MlbTheShowForecaster.GameCards.Apps.MarketplaceWatcher;
 
 var app = AppBuilder.CreateApp(args);
+Console.WriteLine($"Running {FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location).ProductVersion}");
 
 await app.RunAsync();
 
